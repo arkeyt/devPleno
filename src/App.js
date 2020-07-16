6335e07e3b5df0cf844dc8f1634999b07daddcc5
arkeyt <eduardo.setsystem@gmail.com>
2020-07-16 11:48:33 -0300

    "first commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import MostraVoltas from "./MostrarVoltas";
+import MostrarTempo from "./MostrarTempo";
+import Button from "./Button";
+import "./App.css";
 
 function App() {
+  const [numVoltas, setNumVoltas] = useState(14);
+  const [running, setRunning] = useState(false);
+  const [tempo, setTempo] = useState(0);
+
+  const toglleRunning = () => {
+    setRunning(!running);
+  };
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1);
+  };
+
+  const decrement = () => {
+    setNumVoltas(numVoltas - 1);
+  };
+
+  useEffect(() => {
+    let timer = null;
+    if (running) {
+      timer = setInterval(() => {
+        setTempo((old) => old + 1);
+      }, 1000);
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, [running]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MostraVoltas voltas={numVoltas} />
+      <Button text="+" onClick={increment} />
+      <Button text="-" onClick={decrement} /> <br />
+      {numVoltas > 0 && <MostrarTempo tempo={Math.round(tempo / numVoltas)} />}
+      <Button text="Iniciar" onClick={toglleRunning} />
+      <Button text="Reiniciar" />
     </div>
   );
 }
